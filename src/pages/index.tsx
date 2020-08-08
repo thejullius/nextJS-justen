3dc56fc023a5e1449f5285d34ce446288d418380
Julio Goncalves <julio-goncalves@outlook.com>
2020-08-08 12:33:29 -0300

    chore: test config

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import styles from '../../styles/Home.module.css'
 
 export default function Home() {
   return (
@@ -35,7 +35,7 @@ export default function Home() {
             className={styles.card}
           >
             <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
+            <p>Discovwwer and deploy boilerplate example Next.js projects.</p>
           </a>
 
           <a
