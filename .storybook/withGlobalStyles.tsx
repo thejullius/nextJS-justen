b06b6aae9019b166c6081ff0010589db0db475c5
Julio Goncalves <julio-goncalves@outlook.com>
2020-08-08 18:01:40 -0300

    chore: add storybook

---
+++ .storybook/withGlobalStyles.tsx
@@ -0,0 +1,11 @@
+import GlobalStyles from '../src/styles/global'
+import { StoryFn } from '@storybook/addons'
+
+const withGlobalStyles = (storyFn: StoryFn) => (
+  <>
+    <GlobalStyles />
+    {storyFn()}
+  </>
+)
+
+export default withGlobalStyles
--- src/components/Main/test.tsx
+++ src/components/Main/test.tsx
@@ -1,6 +1,6 @@
 import { render, screen } from '@testing-library/react'
 
-import Main from '.'
+import Main from './index'
 
 describe('<Main />', () => {
   it('should render the heading', () => {
@@ -11,5 +11,11 @@ describe('<Main />', () => {
     ).toBeInTheDocument()
 
     expect(container.firstChild).toMatchSnapshot()
+  })
+
+  it('should render the colors correctly', () => {
+    const { container } = render(<Main />)
+
+    expect(container.firstChild).toHaveStyle({ 'background-color': '#06092b' })
   })
 })
